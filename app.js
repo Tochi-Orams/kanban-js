7ed3ebfe719ac3370d6deae117a16ca364de0ac8
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-01-11 15:55:35 +0300

    refactor the  addNewTask's func

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const buttonAdd = document.querySelector('.add')
 const listItemsToDo = document.querySelector('.list-items.toDo')
 
+function createElement(element, className) {
+    const newElement = document.createElement(element)
+    newElement.classList.add(className)
+    newElement.innerText = 'value'
+    return newElement
+}
+
 function addNewTask(){
-    const li = document.createElement('li')
-    li.classList.add('list-item')
-    li.innerText = 'Task'
+    const li = createElement('li','list-item')
     listItemsToDo.insertBefore(li,buttonAdd)
 }
 
