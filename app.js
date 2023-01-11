6528301369fa7b3d5ba4c1d52be402a77c04b9b8
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-01-11 18:13:50 +0300

    features: add a remove task

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const listItemsToDo = document.querySelector('.list-items.toDo')
 
 function createElement(element, className) {
     const newElement = document.createElement(element)
-    newElement.classList.add(className)
-    newElement.innerText = 'value'
+    if(className) newElement.classList.add(className)
+    if(element != 'li') newElement.innerText = className
     return newElement
 }
 
-function addNewTask(){
-    const li = createElement('li','list-item')
-    listItemsToDo.insertBefore(li,buttonAdd)
+function deleteTask() {
+    this.parentElement.remove()
 }
 
+function addNewTask(){
+    const li = createElement('li', 'list-item')
+    const span = createElement('sapn', 'span')
+    const a = createElement('a', 'delete')
+    li.append(span, a)
+    listItemsToDo.insertBefore(li,buttonAdd)
+    a.addEventListener('click', deleteTask)
+}
+
+
 buttonAdd.addEventListener('click', addNewTask)
+
+//const buttonDelete = document.querySelector('.delete')
+console.log(listItemsToDo.childNodes)
+//buttonDelete.addEventListener('click', deleteTask)
