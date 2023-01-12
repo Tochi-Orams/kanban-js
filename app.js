30b61c5f126cc1b7dc8f6bcf4b6b0b6a6691fc9e
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-01-12 21:33:17 +0300

    features(addNewTask): add an input to put task's title

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const buttonAdd = document.querySelector('.add')
+const taskTilte = document.querySelector('#taskTitle')
 const listItemsToDo = document.querySelector('.list-items.toDo')
 
 function createElement(element, className) {
     const newElement = document.createElement(element)
-    if(className) newElement.classList.add(className)
+    if(element != 'span') newElement.classList.add(className)
     if(element != 'li') newElement.innerText = className
     return newElement
 }
@@ -13,17 +14,19 @@ function deleteTask() {
 }
 
 function addNewTask(){
-    const li = createElement('li', 'list-item')
-    const span = createElement('sapn', 'span')
-    const a = createElement('a', 'delete')
-    li.append(span, a)
-    listItemsToDo.insertBefore(li,buttonAdd)
-    a.addEventListener('click', deleteTask)
+    if(taskTilte) {
+        const li = createElement('li', 'list-item')
+        const span = createElement('sapn', taskTilte.value)
+        const a = createElement('a', 'delete')
+        li.append(span, a)
+        listItemsToDo.insertBefore(li,taskTilte)
+        a.addEventListener('click', deleteTask)
+    }
 }
 
 
 buttonAdd.addEventListener('click', addNewTask)
 
 //const buttonDelete = document.querySelector('.delete')
-console.log(listItemsToDo.childNodes)
+//console.log(listItemsToDo.childNodes)
 //buttonDelete.addEventListener('click', deleteTask)
