3da749aad2d0dddbfbd4eeae28ce5132ca0ae053
Thierry Rakotomanana <ThierryRktm@gmail.com>
2023-01-13 11:44:22 +0300

    features(completeTask): add func moveToComplete

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const buttonAdd = document.querySelector('.add')
 const taskTilte = document.querySelector('#taskTitle')
 const listItemsToDo = document.querySelector('.list-items.toDo')
+const listItemsComplete = document.querySelector('.list-items.complete')
+
 
 function createElement(element, className) {
     const newElement = document.createElement(element)
     if(element != 'span') newElement.classList.add(className)
     if(element != 'li') newElement.innerText = className
     return newElement
+}
+
+function moveToComplete(taskComplete){
+    listItemsComplete.append(taskComplete)
 }
 
 function deleteTask() {
@@ -19,7 +25,7 @@ function addNewTask(){
         const span = createElement('span', taskTilte.value)
         const a = createElement('a', 'delete')
         li.append(span, a)
-        listItemsToDo.insertBefore(li,taskTilte)
+        listItemsToDo.insertBefore(li, taskTilte)
         a.addEventListener('click', deleteTask)
     }
 }
